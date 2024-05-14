d4604652f9c3bf1e94a803098c8c0c4059bdd66b
jtyang-beijing <bj.jt.yang@hotmail.com>
2024-05-14 22:25:50 +0800

    change method for Numbers only

--- src/Examples/WpfExample/CustomControl/view/ClearableTextBox.xaml.cs
+++ src/Examples/WpfExample/CustomControl/view/ClearableTextBox.xaml.cs
@@ -53,48 +53,16 @@ namespace FiberPullStrain.CustomControl.view
             else
             {
                 tbPlaceHolder.Visibility = System.Windows.Visibility.Hidden;
+                if(!float.TryParse(inputBox.Text, out float value))
+                {
+                    inputBox.Text="Invalid";
+                }
             }
         }
 
         private void OnPropertyChanged([CallerMemberName]  string propertyName = null) 
         {
             PropertyChanged.Invoke(this, new PropertyChangedEventArgs(propertyName));
-        }
-
-        private void inputBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
-        {
-            e.Handled = !IsTextAllowed(e.Text);
-        }
-        private void inputBox_PreviewKeyDown(object sender, KeyEventArgs e)
-        {
-            // Allow the Delete key
-            if (e.Key == Key.Delete) return;
-
-            // Disallow spaces and other invalid keys
-            if (e.Key == Key.Space || (e.Key < Key.D0 || e.Key > Key.D9) && (e.Key < Key.NumPad0 || e.Key > Key.NumPad9))
-            {
-                e.Handled = true;
-            }
-        }
-        private void inputBox_Pasting(object sender, DataObjectPastingEventArgs e)
-        {
-            if (e.DataObject.GetDataPresent(typeof(string)))
-            {
-                string text = (string)e.DataObject.GetData(typeof(string));
-                if (!IsTextAllowed(text))
-                {
-                    e.CancelCommand();
-                }
-            }
-            else
-            {
-                e.CancelCommand();
-            }
-        }
-        private static bool IsTextAllowed(string text)
-        {
-            Regex regex = new Regex("[^0-9]+"); // Disallow non-numeric characters
-            return !regex.IsMatch(text);
         }
     }
 }
