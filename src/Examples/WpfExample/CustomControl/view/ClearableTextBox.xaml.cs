02f14755946aad0138e096166780e8d7ca124e60
jtyang-beijing <bj.jt.yang@hotmail.com>
2024-05-14 22:09:50 +0800

    Distance and Force Input numbers only

--- src/Examples/WpfExample/CustomControl/view/ClearableTextBox.xaml.cs
+++ src/Examples/WpfExample/CustomControl/view/ClearableTextBox.xaml.cs
@@ -1,6 +1,9 @@
 ﻿using System.ComponentModel;
 using System.Runtime.CompilerServices;
+using System.Text.RegularExpressions;
+using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
 namespace FiberPullStrain.CustomControl.view
 {
     public partial class ClearableTextBox : UserControl, INotifyPropertyChanged
@@ -56,6 +59,42 @@ namespace FiberPullStrain.CustomControl.view
         private void OnPropertyChanged([CallerMemberName]  string propertyName = null) 
         {
             PropertyChanged.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private void inputBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = !IsTextAllowed(e.Text);
+        }
+        private void inputBox_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            // Allow the Delete key
+            if (e.Key == Key.Delete) return;
+
+            // Disallow spaces and other invalid keys
+            if (e.Key == Key.Space || (e.Key < Key.D0 || e.Key > Key.D9) && (e.Key < Key.NumPad0 || e.Key > Key.NumPad9))
+            {
+                e.Handled = true;
+            }
+        }
+        private void inputBox_Pasting(object sender, DataObjectPastingEventArgs e)
+        {
+            if (e.DataObject.GetDataPresent(typeof(string)))
+            {
+                string text = (string)e.DataObject.GetData(typeof(string));
+                if (!IsTextAllowed(text))
+                {
+                    e.CancelCommand();
+                }
+            }
+            else
+            {
+                e.CancelCommand();
+            }
+        }
+        private static bool IsTextAllowed(string text)
+        {
+            Regex regex = new Regex("[^0-9]+"); // Disallow non-numeric characters
+            return !regex.IsMatch(text);
         }
     }
 }
